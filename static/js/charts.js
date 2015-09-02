e851e7d66c19a3af41679d5b5d17156db28647ef
lucasmachadorj <lucasmachado.rj@gmail.com>
2015-09-02 02:06:33 -0300

    primeiros passos com a manipulacao dos dados com crossfilter

--- static/js/charts.js
+++ static/js/charts.js
@@ -5,4 +5,24 @@ queue()
 function makeGraphs(error, crimesjson){
     var crimes = crimesjson;
 
-}
+    //cross filter instance
+    var ndx = crossfilter(crimes);
+
+    //dimensões
+    var municipioDim = ndx.dimension(function(d){ return d["MUNICIPIO"]; });
+    var fatoDim = ndx.dimension(function(d) { return d["FATO"]; });
+    var qtdeDim = ndx.dimension(function(d) { return d["QTDE"]; });
+    var anoDim = ndx.dimension(function(d) { return d["ANO"]; });
+    var mesDim = ndx.dimension(function(d) { return d["MES"]; });
+
+
+    //metricas calculadas
+    var numCrimesPorFato = fatoDim.group(),
+        numCrimesPorMunicipio = municipioDim.group(),
+        numCrimesPorAno = anoDim.group();
+
+    var all = ndx.groupAll();
+
+
+};
+
