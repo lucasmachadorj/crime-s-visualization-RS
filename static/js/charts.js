1ea96b6c996660605f07017445d4e8747642e473
lucasmachadorj <lucasmachado.rj@gmail.com>
2015-09-11 16:27:58 -0300

    d3.js lendo csv com sucesso

--- static/js/charts.js
+++ static/js/charts.js
@@ -9,8 +9,9 @@ var b = { w: 75, h: 30, s: 3, t: 10};
 // Mapping of step names to colors.
 // Mapping of step names to colors.
 var colors = {
-  "mesorregião": "#5687d1",
-  "municipio": "#7b615c",
+  "mes": "#a173d1",
+  "mesorregiao": "#5687d1",
+  "microrregiao": "#7b615c",
   "fato": "#de783b",
 };
 
@@ -33,3 +34,49 @@ var arc = d3.svg.arc()
     .endAngle(function(d) { return d.x + d.dx; })
     .innerRadius(function(d) { return Math.sqrt(d.y); })
     .outerRadius(function(d) { return Math.sqrt(d.y + d.dy); });
+
+d3.text("static/js/path.csv", function(text){
+    var csv = d3.csv.parseRows(text);
+    var json = buildHierarchy(csv);
+});
+
+
+function buildHierarchy(csv){
+    var root = {"name": "root", "children": []};
+    for( var i = 0; i < csv.length; i++){
+        var sequence = csv[i][0];
+        var size = +csv[i][1];
+        if(isNaN(size)){
+            continue;
+        }
+
+        var parts = sequence.split("-");
+        console.log(parts);
+        var currentNode = root;
+        for( var j = 0; j < parts.length; j++){
+            var children = currentNode["children"];
+            var nodeName = parts[j];
+            var childNode;
+            if(j + 1 < parts.length){
+                var foundChild = false;
+                for( var k = 0; k < children.length; k++){
+                    if(children[k]["name"] == nodeName){
+                        childNode = children[k];
+                        foundChild = true;
+                        break;
+                    }
+                }
+
+                if(!foundChild){
+                    childNode = {"name":nodeName, "children": []};
+                    children.push(childNode);
+                }
+                currentNode = childNode;
+            } else {
+                childNode = {"name": nodeName, "size": size};
+                children.push(childNode);
+            }
+        }
+    }
+    return root;
+};
